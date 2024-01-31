c5d2ef5c7791130da5680d0ce4986995d7855800
AerellSmk <aerellsmk2023@gmail.com>
2024-01-31 15:38:17 +0700

    Update And Add: Menambahkan controller main di backend dan menaruh hasil build frontend ke backend

---
+++ backend/resources/views/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="./images/svg/logo.svg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website sekolah SMK Pekanbaru Ravengers">
+    <title>SMK PEKANBARU RAVENGERS</title>
+    
+    <script type="module" crossorigin src="./js/index.js"></script>
+    <link rel="stylesheet" href="./css/index.css">
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
+</html>
